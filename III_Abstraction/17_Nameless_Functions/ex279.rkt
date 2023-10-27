c789081e781ff03acbb9485ceae85eefc25e74e6
jesseck3013 <jesseck3013@outlook.com>
2023-10-27 17:34:52 +0800

    added 17.1

---
+++ III_Abstraction/17_Nameless_Functions/ex279.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex279) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; legal
+(lambda (x y) (x y y))
+
+; illegal
+(lambda () 10)
+
+; legal
+(lambda (x) x)
+
+; legal
+(lambda (x y) x)
+
+; illegal
+(lambda x 10)
+
